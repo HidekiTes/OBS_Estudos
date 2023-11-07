efc8dc72a4ec456a204be0d52b7884bc90449f7d
Pedro Luiz <pedro5353535@gmail.com>
2023-11-07 16:35:15 -0300

    Atualização de Aulas e upload de códigos novos para o Folder

--- 1_Codigos/Codigos_PY/Cod_Python/Aula_Cap_2/04_aula.py
+++ 1_Codigos/Codigos_PY/Cod_Python/Aula_Cap_2/04_aula.py
@@ -14,11 +14,11 @@ local podem ser alcançados
 x = 1
 
 def escopo():
-    global x
+    # global x
     x = 10
 
     def outra_funcao():
-        global x
+        # global x
         x = 11
         y = 2
         print(x, y)
