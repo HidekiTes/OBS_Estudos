352492d41449d971ce77e0c16b57a4c9a364d071
Pedro Luiz <pedro5353535@gmail.com>
2023-11-09 16:37:01 -0300

    Adição das Aulas 24 a 35 no diretório de códigos (Fazer anotações no Obsidian Posteriormente)

---
+++ 1_Codigos/Cod_Python/31_aula.py
@@ -0,0 +1,10 @@
+'''
+Mais detalhes sobre iterables e iterators(iteráveis
+ e iteradores)
+'''
+iterable = ['Eu', 'Tenho','__iter__']
+iterator =  iter(iterable)
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
+# print(next(iterator))
---
+++ 1_Codigos/Cod_Python/35_aula.py
@@ -0,0 +1,6 @@
+'''
+(Parte 1) - Try e Except para tratar exceções
+
+Try, except, else e finally
+'''
+# (Continuar a partir daqui, pois parei na Aula 149)
