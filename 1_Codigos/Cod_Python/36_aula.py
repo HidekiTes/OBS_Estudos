377d89561f6953c53e4fec2b61c5e84d5601e8f9
Pedro Luiz de Barros <pedro5353535@gmail.com>
2023-11-09 23:04:53 -0300

    Atualização de Códigos de Python - Aulas 35 a 38 do Cap 2

--- 1_Codigos/Cod_Python/36_aula.py
+++ 1_Codigos/Cod_Python/36_aula.py
@@ -1,5 +1,5 @@
 '''
-(Parte 1) - Try e Except para tratar exceções
+(Parte 2) - Try e Except para tratar exceções
 
 Try, except, else e finally
 '''
@@ -11,15 +11,18 @@ try:
     a = 18
     b = 0
     # print(b[0])
-    print('Linha 1'[1000])
+    # print('Linha 1'[1000])
     c = a / b
     print('Linha 2')
-except ZeroDivisionError:
-    print('Dividiu por zero.')
+except ZeroDivisionError as e:
+    print(e.__class__.__name__)
+    print(e)
 except NameError:
     print('Nome b não está definido')
-except (TypeError, IndexError):
+except (TypeError, IndexError) as error:
     print('TypeError + IndexError')
+    print('MSG:', error)
+    print('Nome:', error.__class__.__name__)
 except Exception:
     print('ERRO DESCONHECIDO.')
 
